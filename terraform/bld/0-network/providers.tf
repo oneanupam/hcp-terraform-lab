04ad29e7da8e6c81b752a7f01ddaf3d2c6638815
anupam. <43881706+oneanupam@users.noreply.github.com>
2025-11-23 01:57:45 +0530

    feat: kzb-05 bump tf version to 1.14.0 (#5)

--- terraform/bld/0-network/providers.tf
+++ terraform/bld/0-network/providers.tf
@@ -1,8 +1,9 @@
-// Provider credential configuration
+// GA Provider credential configuration
 provider "google" {
   region = var.default_region
 }
 
+// Beta Provider credential configuration
 provider "google-beta" {
   region = var.default_region
 }
