5a7fc6a4126611627466842b6efeec7ae49f1988
Anupam <anupaminit@gmail.com>
2025-11-23 01:56:12 +0530

    feat: kzb-05 bump tf version to 1.14.0

--- terraform/bld/0-network/versions.tf
+++ terraform/bld/0-network/versions.tf
@@ -1,16 +1,16 @@
-// Terraform related configurations
+// Terraform block to configure terraform and provider version
 terraform {
-  required_version = ">= 1.3.6"
+  required_version = "~> 1.14.0"
 
   required_providers {
     # Google Provider Configuration Reference: https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.51.0"
+      version = "~> 7.12.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.51.0"
+      version = "~> 7.12.0"
     }
   }
 }
